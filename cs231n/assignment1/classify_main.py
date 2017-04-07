cc8839942761bc8874603d81257687df09153520
jerryshi <jerryshi0110@hotmail.com>
2017-04-07 18:12:00 +0800

    added a cross validation test for knn classification

--- cs231n/assignment1/classify_main.py
+++ cs231n/assignment1/classify_main.py
@@ -13,6 +13,7 @@ from load_data  import  *
 from k_nearest_neighbor import *
 import numpy as np
 import  time
+import matplotlib.pyplot as plt
 
 def KNN(data_path):
     Xtr, Ytr, Xte, Yte = load_CIFAR10(data_path)
@@ -32,10 +33,46 @@ def KNN(data_path):
 
     print('accuracy: %f' % (np.mean(Yte_pre == Yte)))
 
+def CVKnn(data_path):
+    """
+    Cross Validation to find the best hyperparameter K for neighbors
+    :param data_path: data set path
+    :return: plot the k and accuracy curve
+    """
+
+    # load the full data from dataset
+    Xtr, Ytr, Xte, Yte = load_CIFAR10(data_path)
+    # flatten all image data to one-dimension
+    Xtr_row = Xtr.reshape(Xtr.shape[0], 32*32*3) # become 50000*3072
+    Xte_row = Xte.reshape(Xte.shape[0], 32*32*3) # become 10000*3072
+
+    # choose 1000 data as validation , left as training data
+    XtrVal_row = Xte_row[:1000, :]
+    YtrVal_row = Ytr[:1000]
+
+
+    # left 49,000 as train samples
+    Xtr_row = Xte_row[1000:, :]
+    Ytr = Ytr[1000:]
+
+    validation_acc = []
+    nn = NearestNeighbor()
+    for k in [1, 3, 5, 10, 20, 50, 100]:
+        nn.train(Xtr_row, Ytr)
+        pred = nn.predict(XtrVal_row, k = k)
+        acc = np.mean(YtrVal_row == pred)
+        print('accuracy: %f' % (acc))
+        validation_acc.append((k, acc))
+
+    npArr = np.array(validation_acc)
+    plt.plot(npArr[:, 0], npArr[:, 1],  'b-', lw=1.5)
+    plt.show()
+
 
 if __name__ == "__main__":
     dataSet = "D:\\data\\corpus\\cifar-10-batches-py"
-    start_time = time()
-    KNN(dataSet)
-    end_time = time()
-    print("Finshed Knn classification time cost: %0.3fs", (end_time-start_time))
+    start_time = time.time()
+    # KNN(dataSet)
+    CVKnn(dataSet)
+    end_time = time.time()
+    print("Finshed Knn classification time cost: %0.3fs" % (end_time-start_time))
