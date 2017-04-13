143e15a1ec047d5c13af0074b62ac0bfff568808
jerryshi <jerryshi0110@hotmail.com>
2017-04-13 18:31:22 +0800

    tiny

---
+++ cs231n/assignment1/classifier/linear_svm.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author  : Jerry.Shi
+# File    : linear_svm.py
+# PythonVersion: python3.5
+# Date    : 2017/4/13 9:08
+# Software: PyCharm Community Edition
