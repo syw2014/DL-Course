c313a4769c3ec4277739f7db6f5540a5dfb41326
Jerry.Shi <jerryshi0110@hotmail.com>
2017-09-22 11:20:33 +0800

    re-organized folders

---
+++ course/cs231n/assignment1/classifier/k_nearest_neighbor.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author  : Jerry.Shi
+# File    : k_nearest_neighbor.py
+# PythonVersion: python3.5
+# Date    : 2017/4/7 10:11
+# Software: PyCharm Community Edition
+
+import numpy as np
+from collections import Counter
+
+class NearestNeighbor(object):
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        """X is N x D where each row is an example. Y is 1-Dimension with size N"""
+        # the nearest neighbor classifier simply to remember all the training data
+        self.X = X
+        self.y = y
+
+    def predict(self, X, k=1, loop_num=0):
+
+        if loop_num == 0:
+            dists = self.compute_distacne_no_loop(X)
+        elif loop_num == 1:
+            dists = self.compute_distance_one_loop(X)
+        elif loop_num == 2:
+            dists = self.compute_distance_two_loop(X)
+        else:
+            raise ValueError('Invalide value %d for loop number' % loop_num)
+
+        return self.predict_labels(dists, k=k)
+
+
+    # different predict label calculation methods
+    # in this part we only use L2 distance (Euclidean distance), and there are three implementation
+
+    def compute_distance_two_loop(self, X):
+        """
+        compute the distance between every test data with every training data point,so there are two loop
+        :param X: test data, each row is a single sample
+        :return: dists[i, j], the distance between i-th test data and j-th training data
+        """
+        # get test data size
+        num_test = X.shape[0]
+        # get train data size
+        num_train = self.X.shape[0]
+
+        # final result array
+        dists = np.zeros((num_test, num_train))
+
+        # calculate distance i-th test data and j-th training data
+        for i in range(num_test):
+            for j in range(num_train):
+
+                # l1 distance
+                dists[i, j] = np.linalg.norm(self.X[j, :] - X[i, :])
+                # l2 distance, slow
+                # dists[i, j] = np.sqrt(np.sum(np.square(self.X[j, :] - X[i, :])))
+        return dists
+
+    def compute_distance_one_loop(self, X):
+        """
+        compute the distance between each test data point between trainding data with one loop over test data
+        :param X: test data, each row is a single sample
+        :return: dists[i, j], the distance between i-th test data and j-th training data
+        """
+        # get test data size
+        num_test = X.shape[0]
+        num_train = self.X.shape[0]
+        dists = np.zeros((num_test, num_train))
+
+        for i in range(num_test):
+            # l1 distance
+            dists[i, :] = np.linalg.norm(self.X - X[i, :], axis=1)
+
+            # l2 distance
+            # dists[i, :] = np.sum(np.abs(self.X - X[i,:])**2,axis=-1)**(1./2)
+        return dists
+
+    def compute_distacne_no_loop(self, X):
+        """
+        compute distance between test data and training data without explicit loop
+        :param X: test data, each row is a single sample
+        :return: dists[i, j], the distance between i-th test data and j-th training data
+        """
+        num_test = X.shape[0]
+        num_train = self.X.shape[0]
+        dists = np.zeros((num_test, num_train))
+
+        # as we know , test data and traing data are all vectors, so we can use matrix multiplication to complete
+        # l2 distance calculation
+        # Dist = (X_train_matrix - X_test_matrix) ** 2
+        # = X_train_matrix^T * X_train_matrix - 2 * X_train_matrix * X_test_matrix + X_test_matrix^T * X_test_matrix
+
+        M = 2 * np.dot(X, self.X.T)
+        test = np.square(X).sum(axis=1)
+        train = np.square(self.X).sum(axis=1)
+
+        dists = np.sqrt(train  - M + np.matrix(test).T)
+        return dists
+
+    def predict_labels(self, dists, k=1):
+        """
+        get the top k nearest distance then return is't related label as the predict result
+        :param dists: an array num_test * num_train size, each element indicates the distance i-th test datd point between
+                        j-th train data point
+        :param k: number to select top nearest distance
+        :return: predict label
+        """
+        num_test = dists.shape[0]
+        predict = np.zeros(num_test)
+
+        for i in range(num_test):
+            # closest predict y
+            closest_y = []
+            # use np.argsort to get the sorted index of every distance
+            labels = self.y[np.argsort(dists[i, : ])].flatten()
+            if k > len(labels):
+                k = len(labels)
+
+            closest_y = labels[:k]
+
+            # Counter automatically breaks ties the right way (by choosing the smaller label):
+            # >>> Counter([3, 2, 1, 3, 3, 3, 4, 1, 1, 1]).most_common(1)
+            # [(1, 4)]
+            # >>> Counter([1, 2, 3, 1, 1, 1, 4, 3, 3, 3]).most_common(1)
+            # [(1, 4)]
+            # print closest_y.shape
+            c = Counter(closest_y)
+            predict[i] = c.most_common(1)[0][0]
+
+        return predict
---
+++ course/cs231n/assignment1/classify_main.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# Author  : Jerry.Shi
+# File    : classify_main.py
+# PythonVersion: python3.5
+# Date    : 2017/4/7 14:23
+# Software: PyCharm Community Edition
+
+import sys
+# Note: add module path to sys to tell sys where find your module
+sys.path.append('D:\\github\\DL-Course\\cs231n\\assignment1\\classifier')
+
+
+import numpy as np
+import  time
+import matplotlib.pyplot as plt
+from load_data  import  *
+from k_nearest_neighbor import *
+from linear_svm import *
+from gradient_check import *
+from linear_classifier import *
+
+
+def KNN(data_path):
+    Xtr, Ytr, Xte, Yte = load_CIFAR10(data_path)
+
+    # flatten all image data to one-dimension
+    Xtr_row = Xtr.reshape(Xtr.shape[0], 32*32*3) # become 50000*3072
+    Xte_row = Xte.reshape(Xte.shape[0], 32*32*3) # become 10000*3072
+
+    # create nn classifier
+    nn = NearestNeighbor()
+
+    # train, which means to record all the words
+    nn.train(Xtr_row, Ytr)
+
+    # predict
+    Yte_pre = nn.predict(Xte_row)
+
+    print('accuracy: %f' % (np.mean(Yte_pre == Yte)))
+
+def CVKnn(data_path):
+    """
+    Cross Validation to find the best hyperparameter K for neighbors
+    :param data_path: data set path
+    :return: plot the k and accuracy curve
+    """
+
+    # load the full data from dataset
+    Xtr, Ytr, Xte, Yte = load_CIFAR10(data_path)
+    # flatten all image data to one-dimension
+    Xtr_row = Xtr.reshape(Xtr.shape[0], 32*32*3) # become 50000*3072
+    Xte_row = Xte.reshape(Xte.shape[0], 32*32*3) # become 10000*3072
+
+    # choose 1000 data as validation , left as training data
+    XtrVal_row = Xte_row[:1000, :]
+    YtrVal_row = Ytr[:1000]
+
+
+    # left 49,000 as train samples
+    Xtr_row = Xte_row[1000:, :]
+    Ytr = Ytr[1000:]
+
+    validation_acc = []
+    nn = NearestNeighbor()
+    for k in [1, 3, 5, 10, 20, 50, 100]:
+        nn.train(Xtr_row, Ytr)
+        pred = nn.predict(XtrVal_row, k = k)
+        acc = np.mean(YtrVal_row == pred)
+        print('accuracy: %f' % (acc))
+        validation_acc.append((k, acc))
+
+    npArr = np.array(validation_acc)
+    plt.plot(npArr[:, 0], npArr[:, 1],  'b-', lw=1.5)
+    plt.show()
+
+def svm_classifier(dataset):
+    """
+    Classsify images to their categories by linear svm model
+    :param dataset: training and test data path
+    :return: Nope
+    """
+
+    # step 1, load dataset
+    if len(dataSet) == 0:
+        print("Require no-empty dataset but found %s") % (dataSet)
+        sys.exit(-1)
+    # load CIFAR10 is a util tool function in load_data.py
+    X_train, y_train, X_test, y_test = load_CIFAR10(dataSet)
+
+    # print the shape of every data
+    print("Training data shape: ", X_train.shape)
+    print("Training labels shape: ", y_train.shape)
+    print("Test data shape: ", X_test.shape)
+    print("Test labels shape: ", y_test.shape)
+
+
+    # step 2, print training data
+    # visualize images for every label, we only show 7 imgs for every label
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    num_classes = len(classes)
+    samples_per_class = 7
+    for y, cls in enumerate(classes):
+        # find the index of samples label == y in training samples
+        idxs = np.flatnonzero(y_train == y)
+        # random choose samples
+        idxs = np.random.choice(idxs, samples_per_class, replace=False)
+
+        for i, idx in enumerate(idxs):
+            plt_idx = i * num_classes + y + 1
+            plt.subplot(samples_per_class, num_classes, plt_idx)
+            plt.imshow(X_train[idx].astype('uint8'))
+            plt.axis('off')
+            if i == 0:
+                plt.title(cls)
+    # plt.show()
+
+    # step 3,preprocessing, subsampling
+    num_training = 49000
+    num_validation = 1000
+    num_test = 1000
+
+    # validation dataset
+    mask = range(num_training, num_training + num_validation)
+    # validataion data
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+
+    # train set
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+
+    # test set
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # print size information for every set
+    print("Training data shape: ", X_train.shape)
+    print("Cross validation triaining data shape: ", X_val.shape)
+    print("Cross validation training labels shape: ", y_val.shape)
+    print("Test data shape: ", X_test.shape)
+    print("Test labels shaple: ", y_test.shape)
+
+    # step 4, preprocessing, reshape the image data into rows
+    X_train = np.reshape(X_train, (X_train.shape[0], -1))
+    X_test = np.reshape(X_test, (X_test.shape[0], -1))
+    X_val = np.reshape(X_val, (X_val.shape[0], -1))
+
+    print("Training data shape: ", X_train.shape)
+    print("Test data shape: ", X_test.shape)
+    print("Cross validation data shape: ", X_val.shape)
+
+    # step 5.1, preprocessing, substract the mean img
+    mean_img = np.mean(X_train, axis=0)
+    print(mean_img[:10])
+
+    # image show
+    plt.figure(figsize=(4, 4))
+    plt.imshow(mean_img.reshape((32,32,3)).astype('uint8'))
+
+    # step 5.2, subtract the mean image from train and test data
+    X_train -= mean_img
+    X_test -= mean_img
+    X_val -= mean_img
+
+    # step 5.3,  append the bias dimension of ones (i.e. bias trick) so that our SVM
+    # only has to worry about optimizing a single weight matrix W.
+    # Also, lets transform both data matrices so that each image is a column.
+    X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))]).T
+    X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))]).T
+    X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))]).T
+
+    print("After appending bias, new shape are: ", X_train.shape, X_test.shape, X_val.shape)
+
+    # step 6, classify
+    # generate a random svm weight of small numbers
+    W = np.random.randn(num_classes, X_train.shape[0]) * 0.0001
+
+    # coumpute time cost
+    time1 = time.time()
+    loss, grad = svm_loss_naive(W, X_train, y_train, 0.00001)
+    time2 = time.time()
+    print("SVM naive training completed, loss: %f, computed in %fs" %  (loss, time2 - time1))
+    # gradient check
+    f = lambda w: svm_loss_naive(w, X_train, y_train, 0.0)[0]
+    grad_numerical = grad_check_sparse(f, W, grad, 10)
+
+    # vectorized svm model train
+    time1 = time.time()
+    loss_vectorized, grad_vectorized= svm_loss_vectorized(W, X_train, y_train, 0.00001)
+    time2 = time.time()
+    print("vectorized loss: %f computed in %fs" % (loss_vectorized, time2 - time1))
+    print("difference %f " % (loss_vectorized - loss))
+
+    # difference
+    difference = np.linalg.norm(grad - grad_vectorized, ord='fro')
+    print("difference between two: %f" % (difference))
+
+    #===================
+    # a completed ML system include model train and predict
+    # SGD
+    svm = LinearSVM()
+    start_time = time.time()
+    loss_hist = svm.train(X_train, y_train, learning_rate=1e-7, reg=5e4, num_iters=1500, verbose=True)
+    end_time = time.time()
+    print("completd model train in %fs" % (end_time - start_time))
+
+    # plot loss
+    plt.plot(loss_hist)
+    plt.xlabel("Iteration number")
+    plt.ylabel("Loss value")
+    plt.show()
+
+    # Write the LinearSVM.predict function and evaluate the performance on both the
+    # training and validation set
+    y_train_pred = svm.predict(X_train)
+    print ("training accuracy: %f" % (np.mean(y_train == y_train_pred)))
+    y_val_pred = svm.predict(X_val)
+    print("validation accuacy: %f" % (np.mean(y_val == y_val_pred)))
+
+if __name__ == "__main__":
+    dataSet = "D:\\data\\corpus\\cifar-10-batches-py"
+    # start_time = time.time()
+    # # KNN(dataSet)
+    # CVKnn(dataSet)
+    # end_time = time.time()
+    # print("Finshed Knn classification time cost: %0.3fs" % (end_time-start_time))
+
+    # linear svm
+    # start_time = time.time()
+    svm_classifier(dataSet)
+    # end_time = time.time()
+    # print("Finshed Knn classification time cost: %0.3fs" % (end_time-start_time))
+
